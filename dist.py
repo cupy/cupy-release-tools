1d3ef26e6e2382da7ca04d8e8f0c108a5891f88c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-10-19 19:35:52 +0900

    add long description to CuPy PyPI page

--- dist.py
+++ dist.py
@@ -15,6 +15,7 @@ import time
 from dist_config import (
     CYTHON_VERSION,
     SDIST_CONFIG,
+    SDIST_LONG_DESCRIPTION,
     WHEEL_LINUX_CONFIGS,
     WHEEL_WINDOWS_CONFIGS,
     WHEEL_PYTHON_VERSIONS,
@@ -239,8 +240,9 @@ class Controller(object):
             asset_dest_name = asset_name
             image_tag = 'cupy-builder-sdist'
             base_image = SDIST_CONFIG['image']
+            package_name = 'cupy'
             nccl_config = SDIST_CONFIG['nccl']
-            long_description = None
+            long_description = SDIST_LONG_DESCRIPTION
             assert nccl_config is not None
         else:
             raise RuntimeError('unknown target')
@@ -255,15 +257,17 @@ class Controller(object):
         if nccl_config:
             agent_args += ['--nccl', 'nccl']
 
+        # Add arguments to pass to setup.py.
+        setup_args = [
+            '--cupy-package-name', package_name,
+            '--cupy-long-description', '../description.rst',
+        ]
         if target == 'wheel-linux':
             # Add requirements for build.
             for req in WHEEL_PYTHON_VERSIONS[python_version]['requires']:
                 agent_args += ['--requires', req]
 
-            # Add arguments to pass to setup.py.
-            setup_args = [
-                '--cupy-package-name', package_name,
-                '--cupy-long-description', '../description.rst',
+            setup_args += [
                 '--cupy-no-rpath',
             ]
             for lib in WHEEL_LINUX_CONFIGS[cuda_version]['libs']:
