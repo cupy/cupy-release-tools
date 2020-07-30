e322640052e8d1ef2876843a8d3e219867dc2858
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-07-30 15:44:06 +0900

    use CUDA 9.2 as ubuntu 18 image does not exist for CUDA 9.0

--- dist_config.py
+++ dist_config.py
@@ -7,14 +7,14 @@ CYTHON_VERSION = '0.29.21'
 # See descriptions of WHEEL_LINUX_CONFIGS for details.
 # Note that cuDNN and NCCL must be available for sdist.
 SDIST_CONFIG = {
-    'image': 'nvidia/cuda:9.0-cudnn7-devel-centos6',
+    'image': 'nvidia/cuda:9.2-cudnn7-devel-centos6',
     'nccl': {
         'type': 'v2-tar',
         'files': [
             'nccl_2.5.6-1+cuda9.0_x86_64.txz',
         ],
     },
-    'verify_image': 'nvidia/cuda:9.0-cudnn7-devel-{system}',
+    'verify_image': 'nvidia/cuda:9.2-cudnn7-devel-{system}',
     'verify_systems': ['ubuntu18.04'],
 }
 
