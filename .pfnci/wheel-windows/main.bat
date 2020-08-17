7ff9539edb13519ae6138ccbd43ec5fbcbaf5fa4
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-08-18 01:54:02 +0900

    fail when command exits with error

--- .pfnci/wheel-windows/main.bat
+++ .pfnci/wheel-windows/main.bat
@@ -5,14 +5,14 @@ set CUDA=%2
 set BRANCH=%3
 
 :: Install cuDNN
-PowerShell .pfnci\wheel-windows\install_cudnn.ps1 -cuda %CUDA%
+PowerShell .pfnci\wheel-windows\install_cudnn.ps1 -cuda %CUDA% || exit 1
 
 :: Clone CuPy
-git clone --recursive https://github.com/cupy/cupy.git cupy
-git -C cupy checkout %BRANCH%
+git clone --recursive https://github.com/cupy/cupy.git cupy || exit 1
+git -C cupy checkout %BRANCH% || exit 1
 
 :: Build
-PowerShell .pfnci\wheel-windows\build.ps1 -python %PYTHON% -cuda %CUDA%
+PowerShell .pfnci\wheel-windows\build.ps1 -python %PYTHON% -cuda %CUDA% || exit 1
 
 :: Upload wheel to GCS
-gsutil -m cp cupy*.whl gs://tmp-asia-pfn-public-ci/cupy-release-tools/build-windows/%FLEXCI_JOB_ID%_%PYTHON%_%CUDA%/
+gsutil -m cp cupy*.whl gs://tmp-asia-pfn-public-ci/cupy-release-tools/build-windows/%FLEXCI_JOB_ID%_%PYTHON%_%CUDA%/ || exit 1
