628c377e6727769c9c1cf8c6c79f531440cc5dcc
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-08-18 01:57:33 +0900

    create directory for each run

--- .pfnci/wheel-windows/main.bat
+++ .pfnci/wheel-windows/main.bat
@@ -3,16 +3,17 @@
 set PYTHON=%1
 set CUDA=%2
 set BRANCH=%3
+set JOB_GROUP=%4
 
 :: Install cuDNN
 PowerShell .pfnci\wheel-windows\install_cudnn.ps1 -cuda %CUDA% || exit 1
 
 :: Clone CuPy
 git clone --recursive https://github.com/cupy/cupy.git cupy || exit 1
-git -C cupy checkout %BRANCH% || exit 1
+git -C cupy checkout origin/%BRANCH% || exit 1
 
 :: Build
 PowerShell .pfnci\wheel-windows\build.ps1 -python %PYTHON% -cuda %CUDA% || exit 1
 
 :: Upload wheel to GCS
-gsutil -m cp cupy*.whl gs://tmp-asia-pfn-public-ci/cupy-release-tools/build-windows/%FLEXCI_JOB_ID%_%PYTHON%_%CUDA%/ || exit 1
+gsutil -m cp cupy*.whl gs://tmp-asia-pfn-public-ci/cupy-release-tools/build-windows/%JOB_GROUP%_%BRANCH%/%FLEXCI_JOB_ID%_%PYTHON%_%CUDA%/ || exit 1
