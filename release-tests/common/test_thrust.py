b4d511d47cf0c559fbe98027f2d46908d281fc27
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-08-27 03:25:18 +0900

    update tests for v8

--- release-tests/common/test_thrust.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import cupy
-import cupy.cuda.thrust as libthrust  # NOQA
-
-
-class TestThrust(unittest.TestCase):
-    def test_enabled(self):
-        assert cupy.cuda.thrust_enabled
