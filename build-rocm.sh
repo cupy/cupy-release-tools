07e0a78e3e6763741e51652e16c0fe33b0ce51d6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-02-23 17:05:17 +0000

    warn HCC_AMDGPU_TARGET

--- build-rocm.sh
+++ build-rocm.sh
@@ -3,6 +3,10 @@
 CUDA="${1}"
 PYTHON="${2}"
 
+if [ "${HCC_AMDGPU_TARGET:-}" = "" ]; then
+    echo "*** HCC_AMDGPU_TARGET is not set, verification will fail"
+fi
+
 DIST_OPTIONS="--target wheel-linux --python ${PYTHON} --cuda ${CUDA}"
 eval $(./get_dist_info.py ${DIST_OPTIONS} --source cupy)
 
