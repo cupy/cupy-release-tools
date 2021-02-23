15aa1c5b3c7f121e7e642dbfda8d03a143a53246
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-02-24 00:22:04 +0900

    docker uses tmpfs by default

--- .pfnci/test.sh
+++ .pfnci/test.sh
@@ -6,11 +6,6 @@ PYTHON="$1"
 CUDA="$2"
 BRANCH="$3"
 
-# Mount tmpfs to docker's root directory to speed up.
-service docker stop
-mount -t tmpfs -o size=100% tmpfs /var/lib/docker
-service docker start
-
 # Download NCCL
 ./download_nccl.sh "${CUDA}"
 ls -al nccl
