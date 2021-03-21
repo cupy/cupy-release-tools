63c4a07a890cabd594a0fc05b26f47081535fbd1
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-21 07:33:23 +0000

    flake8

--- dist.py
+++ dist.py
@@ -5,9 +5,7 @@ import argparse
 import json
 import os
 import pathlib
-import random
 import shutil
-import string
 import subprocess
 import sys
 import tempfile
@@ -238,7 +236,7 @@ class Controller(object):
             template = 'debian'
         else:
             raise RuntimeError(
-                'cannot detect OS from image name: '.format(base_image))
+                'cannot detect OS from image name: {}'.format(base_image))
         shutil.copy2(
             '{}/Dockerfile.{}'.format(docker_ctx, template),
             '{}/Dockerfile'.format(docker_ctx))
@@ -390,7 +388,7 @@ class Controller(object):
 
             # Copy builder directory to working directory.
             docker_ctx = '{}/builder'.format(workdir)
-            log('Copying builder directory to: '.format(docker_ctx))
+            log('Copying builder directory to: {}'.format(docker_ctx))
             shutil.copytree('builder/', docker_ctx)
 
             # Extract NCCL archive.
