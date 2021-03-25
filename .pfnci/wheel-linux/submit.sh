ed81878223f1b396724f58573fb31d640bf042f7
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-25 13:25:55 +0900

    Fix version

--- .pfnci/wheel-linux/submit.sh
+++ .pfnci/wheel-linux/submit.sh
@@ -10,7 +10,7 @@ echo "JOB_GROUP = ${JOB_GROUP}"
 echo "URL = https://console.cloud.google.com/storage/browser/tmp-asia-pfn-public-ci/cupy-release-tools/build-linux/${JOB_GROUP}_${BRANCH}/"
 imosci --project=cupy-wheel-linux run ".pfnci/wheel-linux/main.sh 3.7.0 sdist ${BRANCH} ${JOB_GROUP}"
 for CUDA in 9.0 9.2 10.0 10.1 10.2 11.0 11.1 11.2; do
-  for PYTHON in 3.5 3.6.0 3.7.0 3.8.0 3.9.0; do
+  for PYTHON in 3.5.3 3.6.0 3.7.0 3.8.0 3.9.0; do
     imosci --project=cupy-wheel-linux run ".pfnci/wheel-linux/main.sh ${PYTHON} ${CUDA} ${BRANCH} ${JOB_GROUP}"
   done
 done
