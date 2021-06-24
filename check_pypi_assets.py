ae07d04fe2498aad040f3cd1ae89660cc5c360d5
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-06-24 07:27:44 +0000

    update check tool

--- check_pypi_assets.py
+++ check_pypi_assets.py
@@ -25,7 +25,6 @@ _v9_cuda_matrix = list(itertools.product(
 wheel_projects = {
     # v10.x
     '10': [
-        ('cupy-cuda92',   _v10_cuda_matrix),
         ('cupy-cuda100',  _v10_cuda_matrix),
         ('cupy-cuda101',  _v10_cuda_matrix),
         ('cupy-cuda102',  _v10_cuda_matrix),
@@ -33,6 +32,8 @@ wheel_projects = {
         ('cupy-cuda111',  _v10_cuda_matrix),
         ('cupy-cuda112',  _v10_cuda_matrix),
         ('cupy-rocm-4-0',  list(itertools.product(
+            (CP36, CP37, CP38, CP39), (LINUX,)))),
+        ('cupy-rocm-4-2',  list(itertools.product(
             (CP36, CP37, CP38, CP39), (LINUX,)))),
     ],
 
@@ -46,6 +47,8 @@ wheel_projects = {
         ('cupy-cuda111',   _v9_cuda_matrix),
         ('cupy-cuda112',   _v9_cuda_matrix),
         ('cupy-rocm-4-0',  list(itertools.product(
+            (CP36, CP37, CP38, CP39), (LINUX,)))),
+        ('cupy-rocm-4-2',  list(itertools.product(
             (CP36, CP37, CP38, CP39), (LINUX,)))),
     ],
 }
