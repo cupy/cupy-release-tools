6f8950b373a592d5a9e50e805d55d54ac15c03a5
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-08-04 07:03:17 +0000

    fix Python 3.6 versions

--- .pfnci/wheel-windows/submit.sh
+++ .pfnci/wheel-windows/submit.sh
@@ -9,7 +9,7 @@ echo "JOB_GROUP = ${JOB_GROUP}"
 
 echo "URL = https://console.cloud.google.com/storage/browser/tmp-asia-pfn-public-ci/cupy-release-tools/build-windows/${JOB_GROUP}_${BRANCH}/"
 for CUDA in 10.0 10.1 10.2 11.0 11.1 11.2 11.3 11.4; do
-  for PYTHON in 3.6.0 3.7.0 3.8.0 3.9.0; do
+  for PYTHON in 3.6.14 3.7.0 3.8.0 3.9.0; do
     imosci --project=cupy-wheel-win run ".pfnci\\wheel-windows\\main.bat ${PYTHON} ${CUDA} ${BRANCH} ${JOB_GROUP}"
   done
 done
