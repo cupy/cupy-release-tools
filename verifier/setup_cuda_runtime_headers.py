d6d7822796023bb06fa44b7765d28ec5d945583c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2025-08-09 08:00:29 +0000

    install nvidia-cuda-runtime in CUDA 13

--- verifier/setup_cuda_runtime_headers.py
+++ verifier/setup_cuda_runtime_headers.py
@@ -18,15 +18,18 @@ def main() -> None:
     (major, minor) = cupy.cuda.nvrtc.getVersion()
     if major == 11:
         return
-    if major == 12:
+    elif major == 12:
         if minor < 2:
             return
+        suffix = '-cu12'
+    elif major == 13:
+        suffix = ''
     else:
         raise AssertionError(f'Unsupported CUDA version: {major}.{minor}')
 
     cmdline = [
         sys.executable, '-m', 'pip', 'install', '--user',
-        f'nvidia-cuda-runtime-cu{major}=={major}.{minor}.*',
+        f'nvidia-cuda-runtime{suffix}=={major}.{minor}.*',
     ]
     print(f'Running: {shlex.join(cmdline)}')
     subprocess.run(cmdline, check=True)
