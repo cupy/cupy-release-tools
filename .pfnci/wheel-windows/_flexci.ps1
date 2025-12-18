8dfb80a53a2f4d771178a2318e1cc70a7b3cfad8
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2025-12-18 00:46:02 +0000

    sync with cupy repo

--- .pfnci/wheel-windows/_flexci.ps1
+++ .pfnci/wheel-windows/_flexci.ps1
@@ -9,6 +9,8 @@ function ActivatePython($version) {
         $pydir = "Python312"
     } elseif ($version -eq "3.13") {
         $pydir = "Python313"
+    } elseif ($version -eq "3.14") {
+        $pydir = "Python314"
     } else {
         throw "Unsupported Python version: $version"
     }
@@ -60,6 +62,10 @@ function ActivateCUDA($version) {
         throw "Unsupported CUDA version: $version"
     }
     $Env:PATH = "$Env:CUDA_PATH\bin;$Env:CUDA_PATH\bin\x64;" + $Env:PATH
+}
+
+function InstallZLIB() {
+    Copy-Item -Path "C:\Development\ZLIB\zlibwapi.dll" -Destination "C:\Windows\System32"
 }
 
 function IsPullRequestTest() {
