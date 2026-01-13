cbb3a82bb0383c2048dec12446a6288be3eba35a
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2026-01-13 14:44:02 +0000

    use platform push

--- builder/base/cuda-runfile/build.sh
+++ builder/base/cuda-runfile/build.sh
@@ -3,28 +3,32 @@
 set -uex
 
 CUDA="$1"
-IMAGE_SUFFIX="el8"
 BASE_IMAGE="oraclelinux:8"
-PLATFORM="linux/amd64"
 
 case ${CUDA} in
   12.9 )
     CUDA_VERSION="12.9.0"
     CUDA_INSTALLER_URL="https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda_12.9.0_575.51.03_linux.run"
+    PLATFORM="linux/amd64"
+    IMAGE_SUFFIX="el8-amd64"
     ;;
   12.9-aarch64 )
     CUDA_VERSION="12.9.0"
     CUDA_INSTALLER_URL="https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda_12.9.0_575.51.03_linux_sbsa.run"
     PLATFORM="linux/arm64"
+    IMAGE_SUFFIX="el8-aarch64"
     ;;
   13.0 )
     CUDA_VERSION="13.0.0"
     CUDA_INSTALLER_URL="https://developer.download.nvidia.com/compute/cuda/13.0.0/local_installers/cuda_13.0.0_580.65.06_linux.run"
+    PLATFORM="linux/amd64"
+    IMAGE_SUFFIX="el8-amd64"
     ;;
   13.0-aarch64 )
     CUDA_VERSION="13.0.0"
     CUDA_INSTALLER_URL="https://developer.download.nvidia.com/compute/cuda/13.0.0/local_installers/cuda_13.0.0_580.65.06_linux_sbsa.run"
     PLATFORM="linux/arm64"
+    IMAGE_SUFFIX="el8-aarch64"
     ;;
 
   * )
