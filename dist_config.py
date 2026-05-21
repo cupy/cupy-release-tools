ff78321dc588582b467218c5c45c213f5f3e47d0
Kenichi Maehashi <939877+kmaehashi@users.noreply.github.com>
2026-05-22 03:01:24 +0900

    Merge pull request #468 from cupy/copilot/support-cuda-13-1
    Support CUDA 13.1 & 13.2

--- dist_config.py
+++ dist_config.py
@@ -123,7 +123,7 @@ WHEEL_LINUX_CONFIGS: dict[str, _WheelLinuxConfig] = {
         'platform_version': '13.x',
         # Use the latest CUDA version for build.
         # Note: oldest RHEL supported in CUDA 13 is v8
-        'image': 'cupy/cupy-release-tools:cuda-runfile-13.0.0-el8-amd64',
+        'image': 'cupy/cupy-release-tools:cuda-runfile-13.2.1-el8-amd64',
         'libs': [],
         'includes': [],
         'preloads': ['cutensor', 'nccl'],
@@ -131,6 +131,8 @@ WHEEL_LINUX_CONFIGS: dict[str, _WheelLinuxConfig] = {
         'verify_systems': [
             # Test on all supported CUDA version variants.
             '13.0.0-runtime-ubuntu22.04',
+            '13.1.0-runtime-ubuntu22.04',
+            '13.2.1-runtime-ubuntu22.04',
         ],
         'system_packages': '',
     },
@@ -141,7 +143,7 @@ WHEEL_LINUX_CONFIGS: dict[str, _WheelLinuxConfig] = {
         'arch': 'aarch64',
         'platform_version': '13.x',
         # Use the latest image.
-        'image': 'cupy/cupy-release-tools:cuda-runfile-13.0.0-el8-aarch64',
+        'image': 'cupy/cupy-release-tools:cuda-runfile-13.2.1-el8-aarch64',
         'libs': [],
         'includes': [],
         'preloads': ['nccl'],
@@ -150,6 +152,8 @@ WHEEL_LINUX_CONFIGS: dict[str, _WheelLinuxConfig] = {
         'verify_systems': [
             # Test on all supported CUDA version variants.
             '13.0.0-runtime-ubi8',
+            '13.1.0-runtime-ubi8',
+            '13.2.1-runtime-ubi8',
         ],
         'system_packages': '',
     },
@@ -200,7 +204,7 @@ WHEEL_WINDOWS_CONFIGS: dict[str, _WheelWindowsConfig] = {
         'libs': [],
         'preloads': ['cutensor'],
         'cudart_lib': 'cudart64_13',  # binary compatible between CUDA 13.x
-        'check_version': lambda x: 13000 <= x < 13010,  # CUDA 13.0
+        'check_version': lambda x: 13020 <= x < 13030,  # CUDA 13.2
     }
 }
 
